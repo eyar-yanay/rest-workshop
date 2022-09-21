77070d054ff4c71e0698baf16ed3aa038d71479e
eyar-yanay <eyar.yanai@amitnet.carmel6000.org>
2022-09-21 13:11:08 +0300

    app and App are with the answers

--- client/src/AppNoAnswers.js
+++ client/src/AppNoAnswers.js
@@ -12,13 +12,8 @@ function App() {
   const [newStock, setNewStock] = useState(1);
   const [searchFlavor, setSearchFlavor] = useState();
   const [amountFlavor, setAmountFlavor] = useState(1);
-  const [delFlavor, setDelFlavor] = useState();
 
   // handle functions for inputs
-  const handleDelFlavor = (e) => {
-    setDelFlavor(e.target.value)
-  }
-
   const handleFlavorChange = (e) => {
     setSearchFlavor(e.target.value);
   }
@@ -51,29 +46,14 @@ function App() {
 
   const getCustomer = async () => {
     // first mission: get the customer with id 2 from the server by including the id in the url as a parameter
-    const res = await fetch("http://localhost:8000/api/customer/1")
-    const data = await res.json()
-    setCustomer(data)
   }
 
   const updateFlavorAmount = async () => {
     // second mission: buy a flavor by name and quantity using searchFlavor and amountFlavor states. here 
-    const res = await fetch(`http://localhost:8000/api/flavor?amount=${amountFlavor}&flavor=${searchFlavor}`, { method: "PUT" })
-    const data = await res.json()
-    setAllFlavors(data)
   }
 
   const postFlavor = async () => {
     // third mission: add a new flavor - passing amount and flavor name using body
-    const body = { newFlavor: newFlavor, newStock: newStock }
-    const res = await fetch('http://localhost:8000/api/flavor',
-      {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: new Headers({ 'content-type': 'application/json' })
-      })
-    const data = await res.json()
-    setAllFlavors(data)
   }
 
   //fourth mission: create a delete flavor functionality
@@ -82,9 +62,6 @@ function App() {
   //3. create a handleClick function that sends to server a fetch req to delete selected item
   //4. tip: use any one of the methods that was used during this exercise (param, query or body)
   const deleteFlavor = async () => {
-    const res = await fetch(`http://localhost:8000/api/customer/exampleParam/${delFlavor}`, { method: "DELETE" })
-    const data = await res.json()
-    setAllFlavors(data)
   }
 
 
@@ -136,18 +113,8 @@ function App() {
             {allFlavors.map(flavor => {
               return <option value={flavor.name}>{flavor.name}</option>
             })}
-          </select>}z
-          <button onClick={() => { updateFlavorAmount(); getFlavors() }}>update</button>
-        </div>
-
-        <div className="search-flavor">
-          <h4>Remove a flavor:</h4>
-          {allFlavors && <select placeholder='flavor name' value={delFlavor} onChange={handleDelFlavor}>
-            {allFlavors.map(flavor => {
-              return <option value={flavor.name}>{flavor.name}</option>
-            })}
           </select>}
-          <button onClick={() => { deleteFlavor(); getFlavors() }}>delete</button>
+          <button onClick={() => { updateFlavorAmount(); getFlavors() }}>update</button>
         </div>
 
       </div>
