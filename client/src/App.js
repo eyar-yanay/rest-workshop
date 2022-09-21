f2245088eebda88299466c79f86ae1d06dababe0
eyar-yanay <eyar.yanai@amitnet.carmel6000.org>
2022-09-21 12:55:09 +0300

    with answers

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { _ARRAY_OF_NUMBERS, _SERVER_ERROR } from './const';
 
 function App() {
   // states for data from server
-  const [allFlavors, setAllFlavors] = useState();
+  const [allFlavors, setAllFlavors] = useState([]);
   const [customer, setCustomer] = useState();
 
   // state for inputs
@@ -12,8 +12,13 @@ function App() {
   const [newStock, setNewStock] = useState(1);
   const [searchFlavor, setSearchFlavor] = useState();
   const [amountFlavor, setAmountFlavor] = useState(1);
+  const [delFlavor, setDelFlavor] = useState();
 
   // handle functions for inputs
+  const handleDelFlavor = (e) => {
+    setDelFlavor(e.target.value)
+  }
+
   const handleFlavorChange = (e) => {
     setSearchFlavor(e.target.value);
   }
@@ -27,7 +32,7 @@ function App() {
     setNewStock(e.target.value);
   }
 
-  // excute the fetch functions when the component is mounted
+  // execute the fetch functions when the component is mounted
   useEffect(() => {
     getFlavors();
     getCustomer();
@@ -46,15 +51,41 @@ function App() {
 
   const getCustomer = async () => {
     // first mission: get the customer with id 2 from the server by including the id in the url as a parameter
-    const res = await fetch("http://localhost:8000/api/flavor")
+    const res = await fetch("http://localhost:8000/api/customer/1")
+    const data = await res.json()
+    setCustomer(data)
   }
 
   const updateFlavorAmount = async () => {
     // second mission: buy a flavor by name and quantity using searchFlavor and amountFlavor states. here 
+    const res = await fetch(`http://localhost:8000/api/flavor?amount=${amountFlavor}&flavor=${searchFlavor}`, { method: "PUT" })
+    const data = await res.json()
+    setAllFlavors(data)
   }
 
   const postFlavor = async () => {
-    // third mission: add a new flavor - passing amount and flavor name using body  
+    // third mission: add a new flavor - passing amount and flavor name using body
+    const body = { newFlavor: newFlavor, newStock: newStock }
+    const res = await fetch('http://localhost:8000/api/flavor',
+      {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: new Headers({ 'content-type': 'application/json' })
+      })
+    const data = await res.json()
+    setAllFlavors(data)
+  }
+
+  //fourth mission: create a delete flavor functionality
+  //1. create in the client a component to select a flavor from stock
+  //2. create a button that deletes a flavor
+  //3. create a handleClick function that sends to server a fetch req to delete selected item
+  //4. tip: use any one of the methods that was used during this exercise (param, query or body)
+  const deleteFlavor = async () => {
+    console.log('delFlavor: ', delFlavor);
+    const res = await fetch(`http://localhost:8000/api/customer/exampleParam/${delFlavor}`, { method: "DELETE" })
+    const data = await res.json()
+    setAllFlavors(data)
   }
 
 
@@ -102,7 +133,6 @@ function App() {
             })}
           </select>
 
-          {/* <input type="text" placeholder="search flavor" value={searchFlavor} onChange={handleFlavorChange} /> */}
           {allFlavors && <select placeholder='flavor name' value={searchFlavor} onChange={handleFlavorChange}>
             {allFlavors.map(flavor => {
               return <option value={flavor.name}>{flavor.name}</option>
@@ -110,6 +140,17 @@ function App() {
           </select>}z
           <button onClick={() => { updateFlavorAmount(); getFlavors() }}>update</button>
         </div>
+
+        <div className="search-flavor">
+          <h4>Remove a flavor:</h4>
+          {allFlavors && <select placeholder='flavor name' value={delFlavor} onChange={handleDelFlavor}>
+            {allFlavors.map(flavor => {
+              return <option value={flavor.name}>{flavor.name}</option>
+            })}
+          </select>}z
+          <button onClick={() => { deleteFlavor(); getFlavors() }}>delete</button>
+        </div>
+
       </div>
     </div>
   );
